ba1fbd674a8338b9e3b73c597384f2c6c9f5f44b
Quynhnhu <nhu24icloud@gmail.com>
2025-12-15 16:49:45 +0700

    [~] updating code

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
       include: ['src/**/*.{ts,tsx,js,jsx}'], // Specify the file types to lint
     }),
   ],
-  base: '/web/ui-example',
+  base: '/web/ui-example/',
   logLevel: 'info',
   resolve: {
     alias: {
