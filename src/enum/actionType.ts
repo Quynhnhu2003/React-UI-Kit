94c371e8d13647f889887a318b54e1029cd740bf
Quynhnhu2003 <nhu24icloud@gmail.com>
2025-12-17 15:26:14 +0700

    [~] updating code

--- src/enum/actionType.ts
+++ src/enum/actionType.ts
@@ -1,11 +1,3 @@
-export enum ActionType {
-  NAVIGATE = 'navigate',
-  OPEN_DIALOG = 'openDialog',
-  OPEN_WEBVIEW = 'openWebView',
-  FOLLOW_OA = 'follow_oa',
-  DO_ACTION = 'do_action',
-  GO_TO_SCREEN = 'go_to_screen',
-}
-export enum DoActionType {
-  CALL_PHONE = 'CALL_PHONE',
+export enum ActionTypes {
+  Navigate = "navigate",
 }
--- src/types/views/actionType.ts
+++
@@ -1,7 +0,0 @@
-import { ActionType } from "../../enum";
-
-export type Action = {
-  dataAction: string;
-  actionType: `${ActionType}`;
-  data: any;
-};
